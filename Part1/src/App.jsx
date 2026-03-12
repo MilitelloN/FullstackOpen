6f5c26ec4610ad926f544f2ef3ce1c623f49c6b0
MilitelloN <nicolasmilitello99@gmail.com>
2026-03-12 23:26:10 +0000

    Commit for Ex1.6

--- Part1/src/App.jsx
+++ Part1/src/App.jsx
@@ -1,47 +1,42 @@
-const CourseData = (course) => {
-  const courseName = course.courseData.name
-  const courseParts = course.courseData.parts
-  let total = 0
-  courseParts.forEach(e => {
-    total += e.exercises
-  }); 
-  return (
-    <>
-    <h1>{courseName}</h1>
-    <p>{courseParts[0].name} {courseParts[0].exercises}</p>
-    <p>{courseParts[1].name} {courseParts[1].exercises}</p>
-    <p>{courseParts[2].name} {courseParts[2].exercises}</p>
-    <p>Number of exercises {total}</p>
-    </>
-  )
-}
+import { useState } from 'react'
+
+
+
+
 
 
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const [goodReview, setGoodReview] = useState(0)
+  const [neutralReview, seNeutralReview] = useState(0)
+  const [badReview, setBadReview] = useState(0)
+  
+  const buttonHandler = (review,reviewNum) => {
+    if(review == "Good"){
+      const newReview = reviewNum + 1
+      setGoodReview(newReview)
+    } else  if(review == "Neutral"){
+      const newReview = reviewNum + 1
+      seNeutralReview(newReview)
+    }else if(review == "Bad"){
+      const newReview = reviewNum + 1
+      setBadReview(newReview)
+    }
   }
+
+
 
   return (
     <div>
-      <CourseData courseData={course} />
+      <h1>Give feedback</h1>
+      <button onClick={() => buttonHandler("Good",goodReview)}>Good</button>
+      <button onClick={() => buttonHandler("Neutral",neutralReview)}>Neutral</button>
+      <button onClick={() => buttonHandler("Bad",badReview)}>Bad</button>
+      <h1>Statistics</h1>
+      <p>Good {goodReview}</p>
+      <p>Neutral {neutralReview}</p>
+      <p>Bad {badReview}</p>
     </div>
   )
 }
-
 export default App
