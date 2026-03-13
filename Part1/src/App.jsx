894f214e4f4e5efbaa59a9ee8e55acf040ca8b4b
MilitelloN <nicolasmilitello99@gmail.com>
2026-03-13 13:45:17 +0000

    Commit for Ex1.12

--- Part1/src/App.jsx
+++ Part1/src/App.jsx
@@ -1,91 +1,24 @@
 import { useState } from 'react'
 
-const Statistics = (props) => {
-  const total = props.stats.good + props.stats.neutral + props.stats.bad
-  if (total == 0) {
-    return (
-      <>
-        <p>No feedback given</p>
-      </>
-    )
-  } else {
-    return (
-      <>
-        <h1>Statistics</h1>
-        <table>
-          <tbody>
-            <StatisticRow text="Good" value={props.stats.good} />
-            <StatisticRow text="Neutral" value={props.stats.neutral} />
-            <StatisticRow text="Bad" value={props.stats.bad} />
-            <StatisticRow text="All" value={total} />
-            <StatisticRow text="Average" value={total / 3} />
-            <StatisticRow text="Positive" value={props.stats.good / total} />
-          </tbody>
-        </table>
-      </>
-    )
-
-  }
-}
-
-const StatisticRow = (props) => {
-  if (props.text == "Positive") {
-    return (
-      <>
-        <tr>
-          <td>{props.text}</td>
-          <td>{props.value}%</td>
-        </tr>
-      </>
-    )
-  } else {
-    return (
-      <>
-        <tr>
-          <td>{props.text}</td>
-          <td>{props.value}</td>
-        </tr>
-      </>
-    )
-  }
-
-}
-
-const Button = (props) => {
-  return (
-    <>
-      <button onClick={props.handler}>{props.text}</button>
-    </>
-  )
-}
-
-
-
 const App = () => {
-  const [goodReview, setGoodReview] = useState(0)
-  const [neutralReview, seNeutralReview] = useState(0)
-  const [badReview, setBadReview] = useState(0)
-  const buttonHandler = (review, reviewNum) => {
-    if (review == "Good") {
-      const newReview = reviewNum + 1
-      setGoodReview(newReview)
-    } else if (review == "Neutral") {
-      const newReview = reviewNum + 1
-      seNeutralReview(newReview)
-    } else if (review == "Bad") {
-      const newReview = reviewNum + 1
-      setBadReview(newReview)
-    }
-  }
-
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+  const [selected, setSelected] = useState(0)
+  const num = Math.floor(Math.random() * anecdotes.length)
   return (
     <div>
-      <h1>Give feedback</h1>
-      <Button handler={() => buttonHandler("Good", goodReview)} text="Good" />
-      <Button handler={() => buttonHandler("Neutral", neutralReview)} text="Neutral" />
-      <Button handler={() => buttonHandler("Bad", badReview)} text="Bad" />
-      <Statistics stats={{ good: goodReview, neutral: neutralReview, bad: badReview }} />
+      <p>{anecdotes[num]}</p>
+      <button onClick={() => setSelected(selected + 1)}>Next anecdote</button>
     </div>
   )
 }
+
 export default App
